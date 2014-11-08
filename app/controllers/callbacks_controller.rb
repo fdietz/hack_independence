6591066b255ba7bc7faa5364297862af1c09f378
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-08 18:23:40 +0100

    new controller and new route

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,13 @@
+class CallbacksController < ApplicationController
+  def show
+    @provider = params[:provide]
+    case @provider
+      when "spotify":
+        p "Welcome back from spotify"
+        #save the spotify token in the session
+      when "rdio":
+        #save the rdio token in the session
+        redirect_to "imports#show"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ HackIndependence::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  get '/auth/:provider/callback', to: 'imports#show'
+  get '/auth/:provide/callback', to: 'callbacks#show'
 
   resources :imports
   resources :exports
