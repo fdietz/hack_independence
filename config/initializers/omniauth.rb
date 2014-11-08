83b20c2847db8db9ad6cf6af72cec8c99913c349
jocialmedia <githubjo@dethlefs.eu>
2014-11-08 19:34:09 +0100

    added all possible scopes for oauth spotify authorization

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], 
+  scope: 'user-read-private user-library-modify user-library-read streaming playlist-modify-private playlist-modify-public playlist-read-private'
   provider :rdio_oauth2, ENV["RDIO_CLIENT_ID"], ENV["RDIO_CLIENT_SECRET"]
 end
