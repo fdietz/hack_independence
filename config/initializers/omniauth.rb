dc549ccc55824bbc1a969c8d0e37ed012b2fa9b5
jocialmedia <githubjo@dethlefs.eu>
2014-11-08 21:02:10 +0100

    solved merged conflict in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"],
-  scope: 'user-read-private user-library-modify user-library-read streaming playlist-modify-private playlist-modify-public playlist-read-private'
+
+  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'user-read-private user-library-modify user-library-read streaming playlist-modify-private playlist-modify-public playlist-read-private'
   provider :rdio, ENV["RDIO_CLIENT_ID"], ENV["RDIO_CLIENT_SECRET"]
+
 end
