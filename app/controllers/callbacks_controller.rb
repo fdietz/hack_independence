42981ca44624341c97a11db437df7f2538cf07cb
Frederik Dietz <fdietz@gmail.com>
2014-11-09 11:02:39 +0100

    add extra step

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,27 +1,34 @@
 class CallbacksController < ApplicationController
   def show
-    @provider = params[:provide]
-    auth = request.env['omniauth.auth']
-    case @provider
-      when "spotify"
-        cookies[:spotify] = true
-        cookies[:spotify_token] = auth
-        p "*"*50
-        p "Welcome back from spotify"
-        p auth.to_s
-        p "*"*50
-        #save the spotify token in the session
-      when "rdio"
-        cookies[:rdio] = true
-        cookies[:rdio_token] = auth
-        p "*"*50
-        p "Welcome back from rdio"
-        p auth.to_s
-        p "*"*50
-        #save the rdio token in the session
-        #redirect_to "imports#show"
+    if params[:id]
+
+    else
+      @callback = true
+      @provider = params[:provide]
+      auth = request.env['omniauth.auth']
+      case @provider
+        when "spotify"
+          cookies[:spotify] = true
+          cookies[:spotify_token] = auth
+          p "*"*50
+          p "Welcome back from spotify"
+          p auth.to_s
+          p "*"*50
+          #save the spotify token in the session
+        when "rdio"
+          cookies[:rdio] = true
+          cookies[:rdio_token] = auth
+          p "*"*50
+          p "Welcome back from rdio"
+          p auth.to_s
+          p "*"*50
+          #save the rdio token in the session
+          #redirect_to "imports#show"
+      end
+      @spotify = cookies[:spotify]
+      @rdio = cookies[:rdio]
+
+      redirect_to import_url("spotify") and return if @spotify && @rdio
     end
-    @spotify = cookies[:spotify]
-    @rdio = cookies[:rdio]
   end
 end
