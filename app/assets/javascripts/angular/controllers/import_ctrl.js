345c996e467af861b61a167b295ce53ceb66bf5b
Frederik Dietz <fdietz@gmail.com>
2014-11-09 16:05:22 +0100

    fancy animations

--- app/assets/javascripts/angular/controllers/import_ctrl.js
+++ app/assets/javascripts/angular/controllers/import_ctrl.js
@@ -1,16 +1,15 @@
 app.controller("ImportCtrl", ["$scope", "$timeout", "$http", function($scope, $timeout, $http) {
 
   $scope.progress = 0;
-
   $scope.loading = true;
-  function retrievePlaylist() {
 
+  function retrievePlaylist() {
     $http({ method: "get", url: "/imports/spotify", headers: headers }).then(function(response) {
       $scope.loading = false;
-      console.log("playlist get", response.data)
-
       $scope.playlists = response.data;
-      $scope.total     = $scope.playlists.length;
+      console.log("playlists", $scope.playlists);
+
+      $scope.totalPlaylists = $scope.playlists.length;
     });
   }
 
@@ -19,6 +18,13 @@ app.controller("ImportCtrl", ["$scope", "$timeout", "$http", function($scope, $t
   var height;
   var rotationDegree;
 
+  function rad(num) {
+    return num * Math.PI / 180;
+  }
+
+
+  var counter = 0;
+  var i, x;
   function updateUI() {
     var $cursor = $(".cursor");
     var $left   = $(".left");
@@ -28,46 +34,97 @@ app.controller("ImportCtrl", ["$scope", "$timeout", "$http", function($scope, $t
     var positionLeft   = $left.offset();
     var positionRight  = $right.offset();
 
-    var stepSize       = (positionRight.left - positionLeft.left)/10;
+    var stepSize       = (positionRight.left - positionLeft.left)/$scope.totalTracks;
 
     var positionXWithMaxHeight = (positionRight.left - positionLeft.left)/2;
 
-    if ($scope.progress < $scope.total/2) {
-      height -= $scope.progress * 10;
-      rotationDegree += 2;
-    } else {
-      height += ($scope.progress-6) * 10;
-      rotationDegree += 2;
-    }
+
+    var increase = Math.PI;
+    height = Math.sin((($scope.progressForPlaylist/$scope.totalTracks)*increase));
+    rotationDegree += ($scope.progressForPlaylist/$scope.totalTracks)*20;
 
     $cursor.show();
-    $cursor.css({ left: $scope.progress * stepSize, top: height });
+    $cursor.css({ left: $scope.progressForPlaylist * stepSize, top: -height*180 });
     var rotateStr = "rotate(" + rotationDegree + "deg)";
     $cursor.css({ transform: rotateStr });
 
-    console.log("pos", $scope.progress, height, "degree", rotationDegree, rotateStr)
+    console.log("pos", $scope.progressForPlaylist)
   }
 
-  function next() {
-    console.log("next")
+  function resetUI() {
+    var $cursor = $(".cursor");
+    $cursor.hide();
+    $cursor.css({ left: 0 });
+    $cursor.show();
 
-    if ($scope.progress < $scope.total) {
-      $scope.createPlaylist($scope.playlists[$scope.progress]).then(function() {
-        $scope.progress += 1;
+    $scope.progressForPlaylist = 0;
+    rotationDegree = -10;
+    height = 0;
+  }
+
+  var currentPlayListTracks = [];
+  function nextTrack() {
+    console.log("nextTrack", $scope.progressForPlaylist, $scope.totalTracks);
+
+    if ($scope.progressForPlaylist < $scope.totalTracks) {
+      var track = $scope.playlists[$scope.progress].tracks[$scope.progressForPlaylist];
+      console.log('track', track)
+      searchTrackId(track).then(function(response) {
+        if (response.data["track_id"]) {
+          currentPlayListTracks.push(response.data["track_id"]);
+        }
+
+        $scope.progressForPlaylist += 1;
         updateUI();
-        next();
+        nextTrack();
       });
     } else {
-      $cursor.css({ transform: "rotate(" + 0 + "deg)" });
-      $cursor.hide();
+      console.log("last track")
 
+      $scope.createPlaylist({ name: $scope.playlists[$scope.progress].name, track_ids: currentPlayListTracks }).then(function(playlist) {
+        console.log("create playlist", playlist)
+
+        $scope.progress += 1;
+        currentPlayListTracks = [];
+        resetUI();
+        nextPlaylist();
+      });
+    }
+  }
+
+  $scope.currentPlaylist = function() {
+    return $scope.playlists[$scope.progress].name;
+  };
+
+  $scope.currentSong = function() {
+    return $scope.playlists[$scope.progress].tracks[$scope.progressForPlaylist].name;
+  };
+
+  $scope.currentArtist = function() {
+    return $scope.playlists[$scope.progress].tracks[$scope.progressForPlaylist].artists;
+  };
+
+  function nextPlaylist() {
+    if ($scope.progress < $scope.totalPlaylists) {
+
+      $scope.progressForPlaylist = 0;
+      $scope.totalTracks = $scope.playlists[$scope.progress].tracks.length;
+
+      console.log("nextPlaylist", $scope.progress, "total tracks", $scope.totalTracks);
+      nextTrack();
+    } else {
       $scope.importDone = true;
 
       $timeout(function() {
         $(".progress-container").addClass("hide");
       }, 1000);
-
     }
+  }
+
+  function searchTrackId(track) {
+    var headers = { 'X-CSRF-Token': $("html").data("authenticity-token") };
+    var params = track;
+    return $http({ method: "get", url: "/exports/rdio", params: params, headers: headers });
   }
 
   $scope.startImport = function() {
@@ -80,7 +137,7 @@ app.controller("ImportCtrl", ["$scope", "$timeout", "$http", function($scope, $t
 
     $(".progress-container").removeClass("hide");
 
-    next();
+    nextPlaylist();
   };
 
   $scope.importAllButtonLabel = function() {
@@ -88,17 +145,17 @@ app.controller("ImportCtrl", ["$scope", "$timeout", "$http", function($scope, $t
       if ($scope.loading) {
         return "Loading playlists";
       } else {
-        return "Import all " + $scope.total + " playlists";
+        return "Import all " + $scope.totalPlaylists + " playlists";
       }
 
     } else if ($scope.importDone) {
       return "I'm done!";
     } else {
-      return "Importing " + $scope.progress + "/" + $scope.total + " ...";
+      return "Importing " + $scope.progress + "/" + $scope.totalPlaylists + " ...";
     }
   };
 
-  var playlist = { playlist: { name: "test", tracks: [ { artist: "Michael Jackson", song: "Bad" }] } };
+  // var playlist = { playlist: { name: "test", tracks: [ { artist: "Michael Jackson", song: "Bad" }] } };
 
   var headers = { 'X-CSRF-Token': $("html").data("authenticity-token") };
   $scope.createPlaylist = function(playlist) {
--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -2,6 +2,30 @@ class ExportsController < ApplicationController
 
   respond_to :json
 
+  def show
+    p cookies[:rdio_oauth_token].to_s
+    p cookies[:rdio_oauth_token_secret].to_s
+    rdio_api = RdioApi.new(:consumer_key => ENV["RDIO_CLIENT_ID"],
+                            :consumer_secret => ENV["RDIO_CLIENT_SECRET"],
+                            :access_token => cookies[:rdio_oauth_token],
+                            :access_secret => cookies[:rdio_oauth_token_secret])
+    p rdio_api.currentUser
+
+    query_string = "#{params[:artists]}, #{params[:name]}"
+    puts "search for #{query_string}"
+    search_result = rdio_api.search(query: query_string, types: "Track")
+    # puts "result: #{search_result.inspect}"
+
+
+    if search_result.present? && search_result["results"].present? && search_result["results"].first.present? && search_result["results"].first["key"]
+      puts "TRACK ID FOUND: #{search_result['results'].first['key']}"
+      render json: { track_id: search_result["results"].first["key"] }
+    else
+      puts "SKIP TRACK: #{query_string}"
+      render json: { status: "error" }
+    end
+  end
+
   def create
     p cookies[:rdio_oauth_token].to_s
     p cookies[:rdio_oauth_token_secret].to_s
@@ -13,23 +37,23 @@ class ExportsController < ApplicationController
 
     # playlist = params[:playlist].with_indifferent_access
     name = params[:name]
-    tracks = params[:tracks]
+    track_ids = params[:track_ids]
 
-    track_ids = []
-    tracks.each do |track|
-      query_string = "#{track[:artists]}, #{track[:name]}"
-      puts "search for #{query_string}"
-      search_result = rdio_api.search(query: query_string, types: "Track")
-      # puts "result: #{search_result.inspect}"
+    # track_ids = []
+    # tracks.each do |track|
+    #   query_string = "#{track[:artists]}, #{track[:name]}"
+    #   puts "search for #{query_string}"
+    #   search_result = rdio_api.search(query: query_string, types: "Track")
+    #   # puts "result: #{search_result.inspect}"
 
 
-      if search_result.present? && search_result["results"].present? && search_result["results"].first.present? && search_result["results"].first["key"]
-        track_ids << search_result["results"].first["key"]
-        puts "TRACK ID FOUND: #{search_result['results'].first['key']}"
-      else
-        puts "SKIP TRACK: #{query_string}"
-      end
-    end
+    #   if search_result.present? && search_result["results"].present? && search_result["results"].first.present? && search_result["results"].first["key"]
+    #     track_ids << search_result["results"].first["key"]
+    #     puts "TRACK ID FOUND: #{search_result['results'].first['key']}"
+    #   else
+    #     puts "SKIP TRACK: #{query_string}"
+    #   end
+    # end
 
     result = rdio_api.createPlaylist(:name => name, description: name, :tracks => track_ids.join(","))
 
