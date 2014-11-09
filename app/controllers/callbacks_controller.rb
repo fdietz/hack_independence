9f83772b6f982b74704041102c7ef2c28a2be2ab
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-09 02:22:45 +0100

    save token for spotif and rdio in cookies

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,15 +1,18 @@
 class CallbacksController < ApplicationController
   def show
     @provider = params[:provide]
+    auth = request.env['omniauth.auth']
     case @provider
       when "spotify"
         cookies[:spotify] = true
+        cookies[:spotify_token] = auth[:credentials][:token]
         p "*"*50
         p "Welcome back from spotify"
         p "*"*50
         #save the spotify token in the session
       when "rdio"
         cookies[:rdio] = true
+        cookies[:rdio_token] = auth[:credentials][:token]
         p "*"*50
         p "Welcome back from rdio"
         p "*"*50
@@ -17,6 +20,8 @@ class CallbacksController < ApplicationController
         #redirect_to "imports#show"
     end
     @spotify = cookies[:spotify]
+    @spotify_token = cookies[:spotify_token]
     @rdio = cookies[:rdio]
+    @rdio_token = cookies[:rdio_token]
   end
 end
